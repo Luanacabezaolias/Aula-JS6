e62b1d73f59f5518cf34d484943c9dca8f2cbb71
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-03 11:20:30 -0300

    aula6

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let opcao = parseInt(prompt("Dgite uma opção"));
+//switvh case
+
+/*let opcao = parseInt(prompt("Dgite uma opção"));
 
 switch(opcao){
     case 1:
@@ -14,5 +16,37 @@ switch(opcao){
             document.write("Nenhuma das opções válidas")
         break;
 
-
 }
+*/
+
+//incremento(++) e decremento(--) 
+/*
+let numero = 7 
+document.write(numero)
+document.write("<br><br>")
+numero = numero+1 
+document.write(numero)
+
+let numero1 = 7 
+document.write(numero1)
+document.write("<br><br>")
+numero1 = numero1-2
+document.write(numero1)
+*/
+
+//for = para 
+
+/*
+for (let i=0; i < 10; i++){
+    document.write("O valor i vai até:" +i);
+    document.write("<br><br>")
+}
+*/
+
+let carros=["Hb20","Jeep","Compass","HRV"]
+for(let i=0; i < carros.length;i++){
+    document.write(carros[i])
+    document.write("<br><br>")
+}
+
+
