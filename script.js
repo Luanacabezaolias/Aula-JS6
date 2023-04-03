5c124e2a2ad428d823f987cf10e4e50b950b2e87
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-03 10:41:51 -0300

    aula6

---
+++ script.js
@@ -0,0 +1,18 @@
+let opcao = parseInt(prompt("Dgite uma opção"));
+
+switch(opcao){
+    case 1:
+        document.write("Primeira opção")
+    break;
+    case 2:
+        document.write("Segunda opção")
+    break;
+    case 3:
+        document.write("Terceira opção")
+    break;
+        default:
+            document.write("Nenhuma das opções válidas")
+        break;
+
+
+}
